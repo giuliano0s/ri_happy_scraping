53262d5f15df8b1ce5d4758f35dddefe4f4fe088
Giuliano3530 <giuliano.souza@gavb.com.br>
2023-09-12 15:44:47 -0300

    uncomented constants

--- Utils/constants.py
+++ Utils/constants.py
@@ -2,95 +2,95 @@ DEPARTMENT_SECTOR = {
     'baby':
             [
                 'alimentacao-do-bebe',
-        #      'banho-e-higiene',
-        #      'brinquedos-para-bebe',
-        #      'passeio-do-bebe',
-        #      'mini-veiculos',
-        #      'saude-e-seguranca',
-        #     'sono-do-bebe'
+             'banho-e-higiene',
+             'brinquedos-para-bebe',
+             'passeio-do-bebe',
+             'mini-veiculos',
+             'saude-e-seguranca',
+            'sono-do-bebe'
             ],
 
-#     'brinquedos':
-#             [
-        #         'artes-e-atividades',
-        #     'blocos-de-montar',
-        #     'bonecas',
-        #     'bonecos-e-cenarios',
-        #     'carrinhos-e-cia',
-        #     'colecionaveis-e-miniaturas',
-        #     'faz-de-conta-e-casinha',
-        #     'instrumentos-musicais',
-        #     'jogos',
-        #     'lançadores',
-        #     'mini-veiculos-eletricos',
-        #     'pelucia-e-plush',
-        #     'pistas-e-autoramas',
-        #     'quebra-cabecas'],
+    'brinquedos':
+            [
+                'artes-e-atividades',
+            'blocos-de-montar',
+            'bonecas',
+            'bonecos-e-cenarios',
+            'carrinhos-e-cia',
+            'colecionaveis-e-miniaturas',
+            'faz-de-conta-e-casinha',
+            'instrumentos-musicais',
+            'jogos',
+            'lançadores',
+            'mini-veiculos-eletricos',
+            'pelucia-e-plush',
+            'pistas-e-autoramas',
+            'quebra-cabecas'],
 
-#     'guloseimas': 
-#             ['doces'],
+    'guloseimas': 
+            ['doces'],
     
-#     'casa':
-#             [#'cama-mesa-e-banho',
-#             'moveis-e-acessorios',
-#             'decoracao',
-#             'utensilios-domesticos'],
+    'casa':
+            ['cama-mesa-e-banho',
+            'moveis-e-acessorios',
+            'decoracao',
+            'utensilios-domesticos'],
 
-#     'esporte':
-#             ['boxe-e-artes-marciais',
-#             'bicicleta',
-#             'futebol-e-futsal',
-#             'volei',
-#             'basquete',
-#             'natacao-e-esportes-aquaticos',
-#             'ping-pong-e-esportes-com-raquete',
-#             'skate-patins-e-patinetes',
-#             'playgrounds',
-#             'outros-esportes'],
+    'esporte':
+            ['boxe-e-artes-marciais',
+            'bicicleta',
+            'futebol-e-futsal',
+            'volei',
+            'basquete',
+            'natacao-e-esportes-aquaticos',
+            'ping-pong-e-esportes-com-raquete',
+            'skate-patins-e-patinetes',
+            'playgrounds',
+            'outros-esportes'],
     
-#     'livros e papelaria':
-#             ['artigos-para-festas',
-#             'bolsas-e-mochilas',
-#             'livros',
-#             'papelaria',
-#             'pilhas-e-baterias',
-#             'souvenirs'],
+    'livros e papelaria':
+            ['artigos-para-festas',
+            'bolsas-e-mochilas',
+            'livros',
+            'papelaria',
+            'pilhas-e-baterias',
+            'souvenirs'],
     
-#     'moda':
-#             [   
-        #         'calçados-infantis',
-        #     'moda-bebe',
-        #     'moda-meninas',
-#             'moda-meninos'],
+    'moda':
+            [   
+                'calçados-infantis',
+            'moda-bebe',
+            'moda-meninas',
+            'moda-meninos'],
     
-#     'pets':
-#             ['acessorios-para-pets',
-#             'brinquedos-para-pets',
-#             'camas-e-capas',
-#             'comer-e-beber',
-#             'higiene',
-#             'roupas-para-pets'],
+    'pets':
+            ['acessorios-para-pets',
+            'brinquedos-para-pets',
+            'camas-e-capas',
+            'comer-e-beber',
+            'higiene',
+            'roupas-para-pets'],
     
-#     'tecnologia':
-#             [
-        #         'caixa-de-som',
-        #     'carregadores-e-acessorios',
-        #     'consoles',
-        #     'controles',
-        #     'fones-de-ouvido',
-        #     'gamer',
-        #     'informatica',
-        #     'jogos-de-video-games',
-        #     'smartphones-e-celulares'],
+    'tecnologia':
+            [
+                'caixa-de-som',
+            'carregadores-e-acessorios',
+            'consoles',
+            'controles',
+            'fones-de-ouvido',
+            'gamer',
+            'informatica',
+            'jogos-de-video-games',
+            'smartphones-e-celulares'],
 
-#     'produtos importados':
-#             [
-        #         'baby',
-        #     'brinquedos',
-        #     'esportes',
-        #     'games',
-        #     'computadores-e-informatica',
-        #     'livros-e-papelaria',
-        #     'eletroportateis']
+    'produtos importados':
+            [
+                'baby',
+            'brinquedos',
+            'esportes',
+            'games',
+            'computadores-e-informatica',
+            'livros-e-papelaria',
+            'eletroportateis']
     }
 
